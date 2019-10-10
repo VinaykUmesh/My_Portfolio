d3992ec543c9d91614959be29314f02f732bd563
umesh <luxurygeek.umesh@gmail.com>
2019-10-10 19:51:12 +0530

    update
    Completed homepage

--- myportfolio/src/component/HomeComponent.js
+++ myportfolio/src/component/HomeComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid,Cell } from 'react-mdl';
+import logo from '../images/logo.png';
 
 
 const HomeComponent = () => {
@@ -7,11 +8,28 @@ const HomeComponent = () => {
         <div style={{width : '100%' ,margin : 'auto'}}>
           <Grid className='grid-gradient'>
               <Cell col={12}>
-                <img src='' alt='avatar' />
+                <img src={logo} alt='avatar'
+                className="avatar-img"
+                />
+                <div className='banner-text'>
+                  <h2>Full Stack Web Developer</h2>
+                    <hr/>
+                    <p>HTML/CSS | Javascript | React Js | Spring Boot | Hibernate | Mysql</p>
+                      <div className='social-img'>
+                      
+                      <a href='/' rel="noopener noreferrer" target='_blank'>
+                      <i className='fa fa-linkedin-square' aria-hidden='true'/>
+                      </a>
+                      <a href='https://github.com/VinaykUmesh' rel="noopener noreferrer" target='_blank'>
+                      <i className='fa fa-github-square' aria-hidden='true'/>
+                      </a>
+                    </div>
+                </div>
               </Cell>
           </Grid>
         </div>
     );
 };
 
+// eslint-disable-next-line 
 export default HomeComponent;
