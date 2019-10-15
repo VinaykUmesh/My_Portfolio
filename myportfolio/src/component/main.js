4269bc2366dd5709b20348098072bc87e9d9fa2b
Vinayk Umesh <luxurygeek.umesh@gmail.com>
2019-10-15 19:35:14 +0530

    Update changes
    Removed Resume component

--- myportfolio/src/component/main.js
+++ myportfolio/src/component/main.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Switch,Route } from 'react-router-dom';
 import HomeComponent from './HomeComponent';
 import AboutMeComponent from './AboutMeComponent';
-import ResumeComponent from './ResumeComponent';
 import ProjectComponent from './ProjectComponent';
 import ContactComponent from './ContactComponent';
 const Main = () => {
     return (
             <Switch>
                 <Route exact path="/" component={HomeComponent} />
-                <Route exact path="/home" component={HomeComponent} />
-                <Route exact path="/resume" component={ResumeComponent} />
+                <Route exact path="/resume" component={HomeComponent} />
                 <Route exact path="/aboutme" component={AboutMeComponent} />
                 <Route exact path="/project" component={ProjectComponent} />
                 <Route exact path="/contactme" component={ContactComponent} />
